56f0d92840cd1e68ff98b551fb45af38cf21a207
jjs523 <jinseong523@gmail.com>
2025-09-30 01:22:36 +0000

    .

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -1,28 +1,40 @@
+// main.c
 #include <stdio.h>
-#include "copy.h"
 #include <string.h>
+#include "copy.h"   // MAXLINE, copy() 제공
 
 char line[MAXLINE];
-char longest[MAXLINE];
 
+int main(void) {
+    char str[5][MAXLINE] = {{0}};
+    int i = 0;
 
-int main(){
-  int len;
-  int max;
-  max = 0;
+    while (i < 5) {
+        if (fgets(line, MAXLINE, stdin) == NULL) break;
+        line[strcspn(line, "\n")] = '\0';
+        copy(line, str[i]);
+        i++;
+    }
+    int n = i;
 
-  while(gets(line) != NULL) {
-    len = strlen(line);
-
-    if(len > max) {
-       max = len;
-       copy(line, longest);
-      }
+    for (int a = 0; a < n - 1; a++) {
+        int max_idx = a;
+        for (int b = a + 1; b < n; b++) {
+            if (strlen(str[b]) > strlen(str[max_idx])) {
+                max_idx = b;
+            }
+        }
+        if (max_idx != a) {
+            char tmp[MAXLINE];
+            copy(str[a], tmp);
+            copy(str[max_idx], str[a]);
+            copy(tmp, str[max_idx]);
+        }
     }
 
-  if(max > 0)
-    printf("%s \n\n\n\n\n", longest);
-
-  return 0;
+    for (i = 0; i < n; i++) {
+        printf("%s\n", str[i]);
+    }
+    return 0;
 }
 
